8471fcdf6f394dfab117e988aa47edd958eb0058
king <kingzheng@can-dao.com>
2019-03-06 16:01:13 +0800

    dockerfile

--- user-service/src/main/java/com/sunchs/lyt/user/controller/LoginController.java
+++ user-service/src/main/java/com/sunchs/lyt/user/controller/LoginController.java
@@ -1,7 +1,7 @@
 package com.sunchs.lyt.user.controller;
 
-import com.sunchs.lyt.framework.bean.RequestData;
-import com.sunchs.lyt.framework.bean.ResultData;
+//import com.sunchs.lyt.framework.bean.RequestData;
+//import com.sunchs.lyt.framework.bean.ResultData;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class LoginController {
 
-    @PostMapping("/setLogin")
-    public ResultData login(@RequestBody RequestData requestData) {
-        ResultData resultData = new ResultData();
-        System.out.println(requestData.token);
-
-        return resultData;
-    }
+//    @PostMapping("/setLogin")
+//    public ResultData login(@RequestBody RequestData requestData) {
+//        ResultData resultData = new ResultData();
+//        System.out.println(requestData.token);
+//
+//        return resultData;
+//    }
 
 }
