376d4972087460431b07c9c66d60eb5c8d67c4eb
king <kingzheng@can-dao.com>
2019-03-07 11:27:10 +0800

    Update AccessFilter.java

--- gateway/src/main/java/com/sunchs/lyt/gateway/filter/AccessFilter.java
+++ gateway/src/main/java/com/sunchs/lyt/gateway/filter/AccessFilter.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class AccessFilter extends ZuulFilter {
     @Override
     public String filterType() {
-        return null;
+        return "pre";
     }
 
     @Override
