ea81b79f76d2137e00b03a7f273863f7edab94f9
king <kingzheng@can-dao.com>
2019-04-10 06:57:35 +0800

    Update QuestionOptionControllerImpl.java

--- question-service/src/main/java/com/sunchs/lyt/question/controller/impl/QuestionOptionControllerImpl.java
+++ question-service/src/main/java/com/sunchs/lyt/question/controller/impl/QuestionOptionControllerImpl.java
@@ -3,6 +3,7 @@ package com.sunchs.lyt.question.controller.impl;
 import com.sunchs.lyt.framework.bean.RequestData;
 import com.sunchs.lyt.framework.bean.ResultData;
 import com.sunchs.lyt.framework.controller.BaseController;
+import com.sunchs.lyt.question.bean.QuestionOptionData;
 import com.sunchs.lyt.question.bean.QuestionOptionParam;
 import com.sunchs.lyt.question.controller.QuestionOptionController;
 import com.sunchs.lyt.question.service.QuestionOptionService;
@@ -19,7 +20,8 @@ public class QuestionOptionControllerImpl extends BaseController implements Ques
     @Override
     public ResultData save(@RequestBody RequestData data) {
         QuestionOptionParam param = data.toObject(QuestionOptionParam.class);
-        return success(questionOptionService.save(param));
+        questionOptionService.save(param);
+        return success();
     }
 
     @Override
