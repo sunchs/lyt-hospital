09294c583adb289c2b841223bd6719b37a1d62a0
king <kingzheng@can-dao.com>
2019-07-21 01:37:41 +0800

    Update HospitalController.java

--- hospital-service/src/main/java/com/sunchs/lyt/hospital/controller/HospitalController.java
+++ hospital-service/src/main/java/com/sunchs/lyt/hospital/controller/HospitalController.java
@@ -23,6 +23,7 @@ public class HospitalController extends BaseController {
      */
     @PostMapping("/save")
     public ResultData save(@RequestBody RequestData data) {
+        System.out.println(data);
         HospitalParam param = data.toObject(HospitalParam.class);
         hospitalService.save(param);
         return success();
