c559a6fc355651b106a31b8d8fbeab870a80f3de
king <kingzheng@can-dao.com>
2019-08-04 06:08:43 +0800

    Update RoleData.java

--- user-service/src/main/java/com/sunchs/lyt/user/bean/RoleData.java
+++ user-service/src/main/java/com/sunchs/lyt/user/bean/RoleData.java
@@ -1,23 +1,8 @@
 package com.sunchs.lyt.user.bean;
 
-public class RoleData {
+import com.sunchs.lyt.db.business.entity.Role;
 
-    public Integer roleId;
-    public String title;
+public class RoleData extends Role {
 
-    public Integer getRoleId() {
-        return roleId;
-    }
 
-    public void setRoleId(Integer roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
