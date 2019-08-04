f8bf2f58bb40d3c106b8841cc7fe6bc8321007fd
king <kingzheng@can-dao.com>
2019-08-04 17:09:54 +0800

    Update NodeService.java

--- user-service/src/main/java/com/sunchs/lyt/user/service/impl/NodeService.java
+++ user-service/src/main/java/com/sunchs/lyt/user/service/impl/NodeService.java
@@ -3,8 +3,15 @@ package com.sunchs.lyt.user.service.impl;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.sunchs.lyt.db.business.entity.Node;
+import com.sunchs.lyt.db.business.entity.Role;
+import com.sunchs.lyt.db.business.entity.RoleNode;
+import com.sunchs.lyt.db.business.entity.UserRole;
 import com.sunchs.lyt.db.business.service.impl.NodeServiceImpl;
+import com.sunchs.lyt.db.business.service.impl.RoleNodeServiceImpl;
+import com.sunchs.lyt.db.business.service.impl.RoleServiceImpl;
+import com.sunchs.lyt.db.business.service.impl.UserRoleServiceImpl;
 import com.sunchs.lyt.framework.util.ObjectUtil;
+import com.sunchs.lyt.framework.util.UserThreadUtil;
 import com.sunchs.lyt.user.bean.MenuData;
 import com.sunchs.lyt.user.bean.NodeData;
 import com.sunchs.lyt.user.dao.ipml.NodeDaoImpl;
@@ -14,7 +21,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class NodeService implements INodeService {
@@ -25,6 +31,15 @@ public class NodeService implements INodeService {
     @Autowired
     private NodeServiceImpl nodeService;
 
+    @Autowired
+    private UserRoleServiceImpl userRoleService;
+
+    @Autowired
+    private RoleServiceImpl roleService;
+
+    @Autowired
+    private RoleNodeServiceImpl roleNodeService;
+
 
     @Override
     public List<NodeData> getList() {
@@ -33,9 +48,15 @@ public class NodeService implements INodeService {
 
     @Override
     public List<MenuData> getMenuList() {
+        List<Integer> userNodeIds = getUserNodeIds();
+        if (userNodeIds.size() == 0) {
+            return new ArrayList<>();
+        }
+
         List<MenuData> list = new ArrayList<>();
         Wrapper<Node> wrapper = new EntityWrapper<Node>()
-                .eq(Node.STATUS, 1);
+                .eq(Node.STATUS, 1)
+                .in(Node.ID, userNodeIds);
         List<Node> nodeList = nodeService.selectList(wrapper);
         nodeList.forEach(n -> {
             if (n.getPid().equals(0)) {
@@ -52,5 +73,36 @@ public class NodeService implements INodeService {
             }
         });
         return list;
+    }
+
+    private List<Integer> getUserNodeIds() {
+        Integer userId = UserThreadUtil.getUserId();
+
+        List<Integer> ruIds = new ArrayList<>();
+        Wrapper<UserRole> userRoleWrapper = new EntityWrapper<UserRole>()
+                .eq(UserRole.USER_ID, userId);
+        userRoleService.selectList(userRoleWrapper).forEach(userRole -> ruIds.add(userRole.getRoleId()));
+        if (ruIds.size() == 0) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> rIds = new ArrayList<>();
+        Wrapper<Role> roleWrapper = new EntityWrapper<Role>()
+                .in(Role.ID, ruIds)
+                .eq(Role.STATUS, 1);
+        roleService.selectList(roleWrapper).forEach(role -> rIds.add(role.getId()));
+        if (rIds.size() == 0) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> rnIds = new ArrayList<>();
+        Wrapper<RoleNode> roleNodeWrapper = new EntityWrapper<RoleNode>()
+                .eq(RoleNode.ROLE_ID, rIds);
+        roleNodeService.selectList(roleNodeWrapper).forEach(roleNode -> rnIds.add(roleNode.getNodeId()));
+        if (rnIds.size() == 0) {
+            return new ArrayList<>();
+        }
+
+        return rnIds;
     }
 }
