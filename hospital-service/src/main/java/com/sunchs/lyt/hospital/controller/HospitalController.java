b52e87bc718277353ef93bcf4829768ae4781628
king <kingzheng@can-dao.com>
2019-08-22 17:33:58 +0800

    Update HospitalController.java

--- hospital-service/src/main/java/com/sunchs/lyt/hospital/controller/HospitalController.java
+++ hospital-service/src/main/java/com/sunchs/lyt/hospital/controller/HospitalController.java
@@ -1,6 +1,5 @@
 package com.sunchs.lyt.hospital.controller;
 
-import com.sunchs.lyt.db.business.entity.Item;
 import com.sunchs.lyt.framework.bean.RequestData;
 import com.sunchs.lyt.framework.bean.ResultData;
 import com.sunchs.lyt.framework.controller.BaseController;
