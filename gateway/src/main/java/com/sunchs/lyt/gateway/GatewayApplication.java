585557149f3af64e88514b4be5a2d778bda6ae46
king <kingzheng@can-dao.com>
2019-12-27 22:42:50 +0800

    Update GatewayApplication.java

--- gateway/src/main/java/com/sunchs/lyt/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sunchs/lyt/gateway/GatewayApplication.java
@@ -2,10 +2,14 @@ package com.sunchs.lyt.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableFeignClients
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
