ee4d826fc16f90d03d1d8af15b867d8910a0c4f1
king <kingzheng@can-dao.com>
2019-12-27 23:22:57 +0800

    测试

--- gateway/src/main/java/com/sunchs/lyt/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sunchs/lyt/gateway/GatewayApplication.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableFeignClients
-@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
