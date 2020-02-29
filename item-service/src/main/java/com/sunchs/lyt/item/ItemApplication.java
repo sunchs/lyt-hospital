5b5177682133ea64eccc5a0ab81231468bda3137
king <kingzheng@can-dao.com>
2020-02-29 15:22:05 +0800

    Update ItemApplication.java

--- item-service/src/main/java/com/sunchs/lyt/item/ItemApplication.java
+++ item-service/src/main/java/com/sunchs/lyt/item/ItemApplication.java
@@ -2,10 +2,35 @@ package com.sunchs.lyt.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication(scanBasePackages = {"com.sunchs.lyt.db","com.sunchs.lyt.item"})
 public class ItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemApplication.class, args);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);//这两句不加不能跨域上传文件，
+        corsConfiguration.setMaxAge(3600l);//加上去就可以了
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
     }
 }
