dc655454fd9a9b2a50489d3cc9b4aef0d754cea3
king <kingzheng@can-dao.com>
2020-06-30 00:51:11 +0800

    Create HospitalFileController.java

---
+++ hospital-service/src/main/java/com/sunchs/lyt/hospital/controller/HospitalFileController.java
@@ -0,0 +1,81 @@
+package com.sunchs.lyt.hospital.controller;
+
+import com.sunchs.lyt.framework.bean.ResultData;
+import com.sunchs.lyt.framework.controller.BaseController;
+import com.sunchs.lyt.hospital.service.impl.HospitalFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/itemFile")
+public class HospitalFileController extends BaseController {
+
+    @Autowired
+    private HospitalFileService hospitalFileService;
+
+    @PostMapping("/upload")
+    public ResultData uploadFile(@RequestParam("file") MultipartFile file) {
+        return success(hospitalFileService.uploadFile(file));
+    }
+
+    @GetMapping("/download")
+    public String downloadFile(@RequestParam("fileName") String fileName, HttpServletResponse response ) {
+        // 获取指定目录下的第一个文件
+        File file = new File(fileName);
+        System.out.println(fileName);
+
+        // 如果文件名存在，则进行下载
+        if (file.exists()) {
+            // 配置文件下载
+            response.setHeader("content-type", "application/octet-stream");
+            response.setContentType("application/octet-stream");
+            // 下载文件能正常显示中文
+            try {
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            // 实现文件下载
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+                System.out.println("Download the song successfully!");
+            }
+            catch (Exception e) {
+                System.out.println("Download the song failed!");
+            }
+            finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
