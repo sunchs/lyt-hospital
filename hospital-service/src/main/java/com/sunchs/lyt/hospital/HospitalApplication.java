9da31175f031c7ea042ed858ec821c4deaf22125
king <kingzheng@can-dao.com>
2020-06-30 16:33:11 +0800

    Update HospitalApplication.java

--- hospital-service/src/main/java/com/sunchs/lyt/hospital/HospitalApplication.java
+++ hospital-service/src/main/java/com/sunchs/lyt/hospital/HospitalApplication.java
@@ -54,7 +54,10 @@ public class HospitalApplication {
     public MultipartResolver multipartResolver() {
 //        log.info("Loading the multipart resolver");
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(50*1024*1024);
+        multipartResolver.setDefaultEncoding("UTF-8");
+        multipartResolver.setResolveLazily(true);//resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
+        multipartResolver.setMaxInMemorySize(40960);
+        multipartResolver.setMaxUploadSize(50*1024*1024);//上传文件大小 50M 50*1024*1024
         return multipartResolver;
     }
 }
